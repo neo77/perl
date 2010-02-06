68f92e4de9bd800d1a48c8a6d0f6e1997ad95a67
neo77 <neo77@panth-net.com>
2010-02-06 21:24:24 +0100

    working

--- FastObject/FastObject.pm
+++ FastObject/FastObject.pm
@@ -11,7 +11,7 @@ package FastObject;
 use strict;
 use warnings;
 
-use Data::Dumper; # _internal_class_Dump
+#use Data::Dumper; # _internal_class_Dump
 #=------------------------------------------------------------------------ USE, CONSTANTS..
 
 our $VERSION = 1.0;
@@ -19,9 +19,8 @@ our $VERSION = 1.0;
 use base 'Exporter';	# exporting functions
 
 our @EXPORT = qw(class has _internal_class_Dump inherite);
-our @INH_CLASS = ();
-our $_INH_CLASS = '';
-my %has_code;
+our $_processed_class = '';		# currently procesed class
+my %_class_code;				# class code hash (class definition for each class
 
 #=------------------------------------------------------------------------ PUBLIC FUNCTIONS (CLASS)
 #
@@ -33,12 +32,12 @@ my %has_code;
 # RETURN: true
 sub class(&;$) {
 	my ($p_code) = @_;
-	my $classname = $_INH_CLASS || caller;
+	my $classname = $_processed_class || caller;
 
-	print "---> \%$classname (".(caller).")\n";
+	#print "---> \%$classname (".(caller).")\n";
 	# create package globals
-	unless ($_INH_CLASS)  {
-		print "Creating $classname (globals)\n";
+	unless ($_processed_class)  {
+		#print "Creating $classname (globals)\n";
 		eval <<EOE;
 			package $classname;
 			use strict;
@@ -46,52 +45,36 @@ sub class(&;$) {
 			our \%attribs;
 			our \$attribs_size = 0;
 			our \$max_objects = 1;
-			our \@objects = ();
-			our \@free_list = (0);
+			our \@objects = 128;
+			our \@free_list;
+			our \%default_param_values;
 
 			sub new {
 				my (\$class,\%params) = \@_;
 			
-				my \$self = (\@free_list) ? ( shift \@free_list ) : \$max_objects++;
+				my \$self = (\@$classname\::free_list) ? ( shift \@$classname\::free_list ) : \$$classname\::max_objects++;
+				for my \$key (keys \%attribs) {
+					\$$classname\::objects[ \$self*\$$classname\::attribs_size + \$$classname\::attribs{"\$key"} ] = \$params{\$key} || \$$classname\::default_param_values{"\$key"};
+				}
 
-				return bless \\\$self, \$_[0];
+				return bless \\\$self, \$class;
 			}
 			sub DESTROY {
-				push \@free_list, \${\$_[0]};
+				push \@$classname\::free_list, \${\$_[0]};
 			}
 EOE
-		print "has_code{$classname} = p_code\n";
-		$has_code{"$classname"} = $p_code;
+		#print "_class_code{$classname} = p_code\n";
+		$_class_code{"$classname"} = $p_code;
+#				for my \$key (keys \%params) {
+#					\$$classname\::objects[ \$self*\$$classname\::attribs_size + \$$classname\::attribs{"\$key"} ] = \$params{\$key} || \$$classname\::default_param_values{"\$key"};
+#				}
 
-=pod
-				for my $key (keys %params) {
-					$objects[ $self*$attribs_size + $attribs{"$key"} ] = $params{$key} || $default[$attribs{"$key"}];
-				}
-				for my $key (keys %params) {
-	#				$object[ $self*$attribs_size + $attribs{"$key"} ] = $params{$key} || $default[$attribs{"$key"}];
-	#			}
-		{"$classname\::new"} = sub { 
-				my ($class,%params) = @_;
+	}
 
-				my $self = (@{"$classname\::free_list"})?(shift @{"$classname\::free_list"}) : ${"$classname\::max_object"}++;
-	#			for my $key (keys %params) {
-	#				$object[ $self*$attribs_size + $attribs{"$key"} ] = $params{$key} || $default[$attribs{"$key"}];
-	#			}
+	#print "runs _class_code ($classname)\n";
+	&{$_class_code{"$classname"}};
 
-				return bless \$self, $_[0];
-				
-		};
-
-		#	sub DESTROY {
-		#		push \@free_list, \${\$_[0]};
-		#	}
-=cut
-}
-
-	print "runs has_code ($classname)\n";
-	&{$has_code{"$classname"}};
-
-	$_INH_CLASS = undef if $_INH_CLASS eq $classname;
+	$_processed_class = undef if $_processed_class eq $classname;
 	# create new
 	# create accessors
 }
@@ -101,12 +84,25 @@ EOE
 #=------
 #* add attribs to class
 # RETURN: put_return_value_here
-sub has($;$$) {
-	my ($attrib_name, $attrib_default, $attrib_type) = @_;
+sub has($;@) {
+	my ($p_attrib_name, %params) = @_;
 
-	my $classname = $_INH_CLASS || caller;
-	print "evaluating: \$$classname\::attribs{$attrib_name} = \$$classname\::attribs_size++; (".(caller).")\n";
-	eval "\$$classname\::attribs{$attrib_name} = \$$classname\::attribs_size++;"
+	my $p_default = $params{'default'};
+
+	my $classname = $_processed_class || caller;
+	##print "evaluating: \$$classname\::attribs{$p_attrib_name} = \$$classname\::attribs_size++; (".(caller).")\n";
+#  inherite
+	eval <<EOE
+		\$$classname\::attribs{$p_attrib_name} = \$$classname\::attribs_size++;;
+			\$$classname\::default_param_values{$p_attrib_name} = \$p_default;
+		package $classname;
+		# inherite check
+		sub $p_attrib_name {
+			(\@_>1)?(\$$classname\::objects[ \${\$_[0]}*\$$classname\::attribs_size + \$$classname\::attribs{"$p_attrib_name"} ] = \$_[1]):
+				\$$classname\::objects[ \${\$_[0]}*\$$classname\::attribs_size + \$$classname\::attribs{"$p_attrib_name"} ];
+		};
+EOE
+			#return \$$classname\::objects[ \$\$self*\$$classname\::attribs_size + \$$classname\::attribs{"$p_attrib_name"} ];
 }
 # TODO (autoACR): update function documentation at header (put_return_value_here)
 
@@ -116,41 +112,41 @@ sub has($;$$) {
 #* inheriting support
 # RETURN: put_return_value_here
 sub inherite {
-	my $inherited = shift;
+	my $p_parent_class = shift;
 	
 	my $classname = caller;
-	print "Inheriting from $inherited ($classname)\n";
+	#print "Inheriting from $p_parent_class ($classname)\n";
 	# create package globals
-	eval "package $classname; use base '$inherited' ";
+	eval "package $classname; use base '$p_parent_class' ";
 
-	$_INH_CLASS = $classname unless $_INH_CLASS;
-	print "runs has_code ($inherited)\n";
-	&{$has_code{"$inherited"}};
+	$_processed_class = $classname unless $_processed_class;
+	#print "runs _class_code ($p_parent_class)\n";
+	&{$_class_code{"$p_parent_class"}};
 }
 # TODO (autoACR): update function documentation at header (put_return_value_here)
 
-
+=pod
 #=-----------------------
 #  _internal_class_Dump
 #=-----------------------
 #* make class dump (internal)
 # RETURN: nothing.. just prints info
 sub _internal_class_Dump($;$) {
-	my ($classname, $extend) = @_;
+	my ($p_classname, $p_extend) = @_;
 	print Dumper({ 
-		attribs 		=> eval "\\\%$classname\::attribs",
-		attribs_size 	=> eval "\$$classname\::attribs_size",
+		attribs 		=> eval "\\\%$p_classname\::attribs",
+		attribs_size 	=> eval "\$$p_classname\::attribs_size",
 		
 	});
-	if ($extend) {
+	if ($p_extend) {
 		print Dumper({ 
-			max_objects => eval "\$$classname\::max_objects",
-			free_list 	=> eval "\\\@$classname\::free_list",
-			objects 	=> eval "\\\@$classname\::objects",
+			max_objects => eval "\$$p_classname\::max_objects",
+			free_list 	=> eval "\\\@$p_classname\::free_list",
+			objects 	=> eval "\\\@$p_classname\::objects",
 		});
 	}
 }
-
+=cut 
 #=------------------------------------------------------------------------ PUBLIC FUNCTIONS (OBJECT)
 
 
@@ -209,29 +205,7 @@ sub x {
 	($_[1]) ? $object[ ${$_[0]}*5 + 0 ] = $_[1] : $object[ ${$_[0]}*5 + 0 ];
 }
 
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
-# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 34)
+# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 87)
+# TODO (autoACR): update function documentation at header (put_return_value_here)
+# FIXME (autoACR): PERL Syntax error sub not closed or { "#" } problem! (start parsing on: 87)
+# TODO (autoACR): update function documentation at header (put_return_value_here)
